a66eaad635026a8ce5116e6a23ecb52f0b4e3ac9
shubhamshahajipol <67409344+shubhamshahajipol@users.noreply.github.com>
2020-08-17 15:24:15 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+alert("Web and App development workshop | Assignment submitted by shubham shahaji pol(193010036 IITB)| Special thanks to Autonise AI workshop Team | Click 'OK' to see his website");
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
